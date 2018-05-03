3600ebc41d42a7e907ee9d5ff52756a67cabbfb7
Oscar <osreama1@gmail.com>
2018-05-03 09:16:07 +0000

    S

--- cookbooks/petclinic/recipes/default.rb
+++ cookbooks/petclinic/recipes/default.rb
@@ -3,26 +3,29 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
 tomcat_install "petclinicserver" do
-  version '8.0.36'
+	version '8.0.36'
 end
 
 tomcat_service "petclinicserver" do
-  action [:enable, :start]
-  env_vars [{'CATALINA_BASE' => '/opt/tomcat_petclinicserver_8_0_36/'},{'CATALINA_PID' => '$CATALINA_BASE/bin/tomcat.pid'},{'JAVA_OPTS' => '-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true'}]
+	action [:enable, :start]
+	env_vars [
+		{'CATALINA_BASE' => '/opt/tomcat_petclinicserver_8_0_36/'},
+		{'CATALINA_PID' => '$CATALINA_BASE/bin/tomcat.pid'},
+		{'JAVA_OPTS' => '-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true'}]
 end
 
 remote_file '/opt/tomcat_petclinicserver_8_0_36/webapps/petclinic.war' do
-  source 'http://104.131.55.44:8081/path/to/petclinic/pettclinic-1.0.0-SNAPSHOT.war'
-  owner 'tomcat_petclinicserver'
-  group 'tomcat_petclinicserver'
+	source http://104.131.55.44:8081/artifactory/webapp/#/artifacts/browse/simple/General/libs-snapshot-local/org/springframework/samples/spring-petclinic/1.0.1-SNAPSHOT.war'
+	owner 'tomcat_petclinicserver'
+	group 'tomcat_petclinicserver'
 end
 
 directory '/home/tomcat_petclinicserver' do
-  owner 'tomcat_petclinicserver'
-  group 'tomcat_petclinicserver'
-  mode 0755
+	owner 'tomcat_petclinicserver'
+	group 'tomcat_petclinicserver'
+	mode 0755
 end
-
-
 
